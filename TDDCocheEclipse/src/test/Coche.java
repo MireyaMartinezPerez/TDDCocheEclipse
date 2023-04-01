13caee3399a6e752469c8f276842cba656449d95
Mireya <thewinlow1@gmail.com>
2023-04-01 21:24:12 +0200

    Segundo commit

--- TDDCocheEclipse/src/test/Coche.java
+++ TDDCocheEclipse/src/test/Coche.java
@@ -2,4 +2,10 @@ package test;
 
 public class Coche {
 
+	public int velocidad;
+
+	public void acelerar(int aceleracion) {
+		velocidad+= aceleracion;
+	}
+
 }
--- TDDCocheEclipse/src/test/TestCoche.java
+++ TDDCocheEclipse/src/test/TestCoche.java
@@ -2,13 +2,21 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TestCoche {
 
 	@Test
-	public void test_crear_coche() {
+	public void test_al_crear_un_coche_su_velocidad_es_cero() {
 		Coche nuevoCoche=new Coche();
+		Assertions.assertEquals(0,nuevoCoche.velocidad);
+	}
+	@Test
+	public void test_al_acelerar_un_coche_su_velocidad_aumenta() {
+		Coche nuevoCoche=new Coche();
+		nuevoCoche.acelerar(30);
+		Assertions.assertEquals(30,nuevoCoche.velocidad);
 	}
 
 }
